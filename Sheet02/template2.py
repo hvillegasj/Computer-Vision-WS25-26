b7cbc80a644b0af74aed59a19ccbc43737c94b07
Heldert <heldert12345@gmail.com>
2025-11-03 23:25:42 +0100

    Developed exercises 2 and 5. In 5 the values were not met

--- Sheet02/template2.py
+++ Sheet02/template2.py
@@ -11,27 +11,129 @@ def compute_fft(img):
     - The magnitude
     - The phase
     """
-    pass
+    #We use numpy module for computing fast fourier transform to calculate the fourier transforms
+    fourier_transform=np.fft.fft2(img)
+
+    #We shift our Fourier transform matrix to get a centered transform.
+    f_shifted = np.fft.fftshift(fourier_transform)
+
+    #Matrix of magnitudes of our fourier transform
+    fourier_transform_magnitude=np.abs(f_shifted)
+
+    #Matrix of phases of our fourier transform
+    fourier_transform_phase=np.angle(f_shifted)
+
+    return f_shifted, fourier_transform_magnitude, fourier_transform_phase
 
 
 def reconstruct_from_mag_phase(mag, phase):
     """
     Reconstruct an image from given magnitude and phase.
     """
-    pass
+    #Recovers the fourier transform of an image by considering the polar expresion of each entry.
+    f_transform=mag*np.exp(1j*phase)
+
+    #Since our inputs will be shifted we need to invert the shift.
+    f_unshifted=np.fft.ifftshift(f_transform)
+
+    img=np.fft.ifft2(f_unshifted)
+
+    return np.real(img)
 
 
 def compute_mad(a, b):
     """
     Compute the Mean Absolute Difference (MAD) between two images.
     """
-    pass
+    #We compute the matrix of differences
+    absolute_difference=np.abs(a-b)
+
+    #We take the mean among all the entries of the previous matrix and return it
+    return np.mean(absolute_difference)
 
 # ==========================================================
+# 1. Load the two grayscale images (1.png and 2.png)
+image_1=cv2.imread("data/1.png",cv2.IMREAD_GRAYSCALE)
+image_2=cv2.imread("data/2.png",cv2.IMREAD_GRAYSCALE)
 
-# TODO: 1. Load the two grayscale images (1.png and 2.png)
-# TODO: 2. Compute magnitude and phase of both images
-# TODO: 3. Swap magnitude and phase between the two images
-# TODO: 4. Reconstruct and save the swapped results
+# 2. Compute magnitude and phase of both images
+transform_img_1, mag_img_1, phase_img_1=compute_fft(image_1)
+transform_img_2, mag_img_2, phase_img_2=compute_fft(image_2)
+
+# 3. Swap magnitude and phase between the two images.
+# 4. Reconstruct and save the swapped results
+reconstruction_1=reconstruct_from_mag_phase(mag_img_2,phase_img_1).astype(np.uint8)
+reconstruction_2=reconstruct_from_mag_phase(mag_img_1,phase_img_2).astype(np.uint8)
+
 # TODO: 5. Compute and print the MAD values between originals and reconstructions
+MAD_1=compute_mad(image_1,reconstruction_1)
+MAD_2=compute_mad(image_2,reconstruction_2)
+
+print(f'The MAD for the first image is {MAD_1:.2f}')
+print(f'The MAD for the second image is {MAD_2:.2f}')
+
 # TODO: 6. Visualize all images (originals, magnitude, phase, reconstructions)
+#Before visualizing the magnitudes and the phases we normalize them by using a logarithm for
+#range correction and normalize to format it before passing to the plot function.
+mag_img_1_log = np.log(1 + mag_img_1)
+mag_img_2_log = np.log(1 + mag_img_2)
+
+mag_img_1_vis=cv2.normalize(mag_img_1_log,None,0,255,cv2.NORM_MINMAX).astype(np.uint8)
+mag_img_2_vis=cv2.normalize(mag_img_2_log,None,0,255,cv2.NORM_MINMAX).astype(np.uint8)
+
+phase_img_1=cv2.normalize(phase_img_1,None,0,255,cv2.NORM_MINMAX).astype(np.uint8)
+phase_img_2=cv2.normalize(phase_img_2,None,0,255,cv2.NORM_MINMAX).astype(np.uint8)
+
+plt.figure(figsize=(10, 4))
+
+plt.subplot(2, 4, 1)
+plt.imshow(image_1, cmap='gray')
+plt.title("Photographer original")
+plt.axis("off")
+
+plt.subplot(2, 4, 2)
+plt.imshow(mag_img_1_vis, cmap='gray')
+plt.title("Photographer magnitude")
+plt.axis("off")
+
+plt.subplot(2, 4, 3)
+plt.imshow(phase_img_1, cmap='gray')
+plt.title("Photographer phase")
+plt.axis("off")
+
+plt.subplot(2, 4, 4)
+plt.imshow(reconstruction_1, cmap='gray')
+plt.title("Photographer with Einstein's phase")
+plt.axis("off")
+
+plt.subplot(2, 4, 5)
+plt.imshow(image_2, cmap='gray')
+plt.title("Einstein original")
+plt.axis("off")
+
+plt.subplot(2, 4, 6)
+plt.imshow(mag_img_2_vis, cmap='gray')
+plt.title("Einstein's magnitude")
+plt.axis("off")
+
+plt.subplot(2, 4, 7)
+plt.imshow(phase_img_2, cmap='gray')
+plt.title("Einstein's phase")
+plt.axis("off")
+
+plt.subplot(2, 4, 8)
+plt.imshow(reconstruction_2, cmap='gray')
+plt.title("Einstein with photographer's phase")
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
+
+#Discussion:
+#The presence of the tripod in the photography of the photographer causes very visible diagonal lines that
+#can be evidenced on its magnitude diagram. In the other hand, Einstein shows clearly a tendency for vertical lines
+#that originate thanks to the courtain in the back of the picture.
+
+# When we mix the images we can kind of see how each picture patterns arise in the other (diagonals in Einstein's and
+# squared in the photographers). Is interesting how even though these change, the phase still manages to keep the images
+# recognizible enough.
--- Sheet02/template5.py
+++ Sheet02/template5.py
@@ -10,7 +10,10 @@ def gaussian_smoothing(img, sigma):
     """
     Apply Gaussian smoothing to reduce noise.
     """
-    pass
+    #We smooth the image using a gaussian filter which changes in size based on the sigma given.
+    smoothed_img=cv2.GaussianBlur(img,(0,0),sigma)
+    
+    return smoothed_img
 
 
 def compute_gradients(img):
@@ -18,22 +21,101 @@ def compute_gradients(img):
     Compute gradient magnitude and direction (Sobel-based).
     Return gradient_magnitude, gradient_angle.
     """
-    pass
+    #We use cv2.Sobel to calculate the partial derivatives of the image.
+    horizontal_derivative=cv2.Sobel(img,cv2.CV_64F,1,0)
+    vertical_derivative=cv2.Sobel(img,cv2.CV_64F,0,1)
+    
+    #We use numpy to compute the angles and the magnitude
+    #Note: There are approximated ways to do it faster but we chose the precise approach using np.
+    gradient_magnitude=np.hypot(horizontal_derivative,vertical_derivative)
+
+    safe_division = np.divide(
+    vertical_derivative.astype(float),
+    horizontal_derivative.astype(float),
+    out=np.zeros_like(vertical_derivative, dtype=float),
+    where=horizontal_derivative != 0
+    ) #Safe division to avoid dividing by 0 when calculating the angle.
+
+    gradient_angle=np.arctan(safe_division)
+    gradient_angle[horizontal_derivative == 0] = np.pi / 2 * np.sign(vertical_derivative[horizontal_derivative == 0])
+
+    return gradient_magnitude, gradient_angle
 
 
 def nonmax_suppression(mag, ang):
     """
     Perform non-maximum suppression to thin edges.
     """
-    pass
+    #Get the shape of the magnitude matrix
+    size=np.shape(mag)
 
+    mag_copy=mag.copy()
 
+    for i in range(1,size[0]-1):
+        for j in range(1,size[1]-1): #Iterate over the pixels in the magnitude matrix
+            theta=ang[i,j]
+            comparing_value_1=0.0
+            comparing_value_2=0.0
+                
+            #We first clasify in which section of the circle is our angle and find the comparing values.
+            if -np.pi/2 <= theta and theta < -np.pi/4:
+                    #Since is pointing downwards we check if the pixels
+                    # needed for the comparison are inbounds and then we
+                    #  perform our interpolation and comparison.
+                w= 1/np.abs(np.tan(theta))
+                
+                #Get our comparing values using a linear interpolation
+                comparing_value_1=w*mag[i,j+1]+(1-w)*mag[i+1,j+1]
+                comparing_value_2=w*mag[i,j-1]+(1-w)*mag[i-1,j-1]
+                        
+            elif -np.pi/4 <= theta and theta < 0:
+                    #Since is pointing rightwards we check if the pixels
+                    # needed for the comparison are inbounds and then we
+                    #  perform our interpolation and comparison.
+                w=np.abs(np.tan(theta))
+
+                #Get our comparing values using a linear interpolation
+                comparing_value_1=w*mag[i+1,j]+(1-w)*mag[i+1,j+1]
+                comparing_value_2=w*mag[i-1,j]+(1-w)*mag[i-1,j-1]
+
+            elif 0 <= theta and theta<np.pi/4:
+                    #Since is pointing rightwards we check if the pixels
+                    # needed for the comparison are inbounds and then we
+                    #  perform our interpolation and comparison.
+                w=np.abs(np.tan(theta))
+                            
+                #Get our comparing values using a linear interpolation
+                comparing_value_1=w*mag[i+1,j]+(1-w)*mag[i+1,j-1]
+                comparing_value_2=w*mag[i-1,j]+(1-w)*mag[i-1,j+1]
+
+            else:
+                    #Since is pointing upwards we check if the pixels
+                    # needed for the comparison are inbounds and then we
+                    #  perform our interpolation and comparison.
+                w=1/np.abs(np.tan(theta))
+
+                #Get our comparing values using a linear interpolation
+                comparing_value_1=w*mag[i,j-1]+(1-w)*mag[i+1,j-1]
+                comparing_value_2=w*mag[i,j+1]+(1-w)*mag[i-1,j+1]
+            
+            #Perform our comparison and set the magnitude value to 0 in case it is not a local maximum
+            if mag[i,j]<comparing_value_1 or mag[i,j]<comparing_value_2:
+                    mag_copy[i,j]=0
+    
+    return mag_copy
+        
+    
 def double_threshold(nms, low, high):
     """
     Apply double thresholding to classify strong, weak, and non-edges.
     Return thresholded edge map.
     """
-    pass
+    img_copy=np.zeros_like(nms)
+
+    img_copy = np.where(nms>high,255,0)+np.where((nms<high) & (nms>low),100,0) 
+    #Ignore everything below the lower threshold and mark as border everything above it
+    
+    return img_copy
 
 
 def hysteresis(edge_map, weak, strong):
@@ -41,23 +123,123 @@ def hysteresis(edge_map, weak, strong):
     Perform edge tracking by hysteresis.
     Return final binary edge map.
     """
-    pass
+    # Identify strong and weak edges
+    strong_edges = (edge_map >= strong).astype(np.uint8)
+    weak_edges = ((edge_map >= weak) & (edge_map < strong)).astype(np.uint8)
+
+    # Initialize final edges with strong edges
+    final_edges = np.copy(strong_edges)
+
+    # 8-connectivity kernel
+    kernel = np.ones((3,3), dtype=np.uint8)
+
+    # Iteratively grow strong edges by connecting weak edges
+    while True:
+        # Dilate strong edges
+        dilated = cv2.dilate(final_edges, kernel, iterations=1)
+
+        # Find weak pixels connected to strong edges
+        connected = np.logical_and(dilated, weak_edges).astype(np.uint8)
+
+        # Add new connections to final edges
+        new_final = np.logical_or(final_edges, connected).astype(np.uint8)
+
+        # Stop if no new edges were added
+        if np.array_equal(new_final, final_edges):
+            break
+
+        final_edges = new_final
+
+    # Scale to 0-255
+    final_edges = final_edges * 255
+
+    return final_edges.astype(np.uint8)
 
 
 def compute_metrics(manual_edges, cv_edges):
     """
     Compute MAD, precision, recall, and F1-score between two binary edge maps.
     """
-    pass
+    #We first normalize the arrays
+    manual_edges=cv2.normalize(manual_edges,None,0,1,cv2.NORM_MINMAX)
+    cv_edges=cv2.normalize(cv_edges,None,0,1,cv2.NORM_MINMAX)
+
+    MAD = np.mean(np.abs(manual_edges-cv_edges))
+
+    size=np.shape(manual_edges)
+
+    tp, fp, tn, fn= 0, 0, 0, 0
+
+    #We go over the whole image to clasify between false/true negative/positive edges detected.
+    for i in range(size[0]):
+        for j in range(size[1]):
+            if manual_edges[i,j]==1 and cv_edges[i,j]==1:
+                tp+=1
+            elif manual_edges[i,j]==1 and cv_edges[i,j]==0:
+                fp+=1
+            elif manual_edges[i,j]==0 and cv_edges[i,j]==0:
+                tn+=1
+            elif manual_edges[i,j]==0 and cv_edges[i,j]==1:
+                fn+=1
+
+    precision = tp / (tp+fp)
+
+    recall = tp / (tp+fn)
+
+    F1_score= tp / (tp + 0.5*(fp + fn))
+
+    return MAD, precision, recall, F1_score
 
 
 # ==========================================================
 
-# TODO: 1. Load the grayscale image 'bonn.jpg'
-# TODO: 2. Smooth the image using your Gaussian function
-# TODO: 3. Compute gradients (magnitude and direction)
-# TODO: 4. Apply non-maximum suppression
-# TODO: 5. Apply double threshold (choose suitable low/high values)
-# TODO: 6. Perform hysteresis to obtain final edges
+#1. Load the grayscale image 'bonn.jpg'
+img=cv2.cvtColor(cv2.imread("data/bonn.jpg"),cv2.COLOR_BGR2GRAY)
+
+#2. Smooth the image using your Gaussian 
+smoothing_factor= 3
+
+img=gaussian_smoothing(img,smoothing_factor)
+
+#3. Compute gradients (magnitude and direction)
+grad_magn,grad_angle=compute_gradients(img)
+
+#4. Apply non-maximum suppression
+maximum_suppresed_image= nonmax_suppression(grad_magn,grad_angle)
+
+#5. Apply double threshold (choose suitable low/high values)
+low_thr=0.22*255
+high_thr=0.38*255
+
+threshholded_image=double_threshold(maximum_suppresed_image,low_thr,high_thr)
+
+#6. Perform hysteresis to obtain final edges
+hysteresis_image=hysteresis(threshholded_image,low_thr,high_thr)
+
 # TODO: 7. Compare your result with cv2.Canny using MAD and F1-score
-# TODO: 8. Display original image, your edges, and OpenCV edges
+cv2_edges=cv2.Canny(img,low_thr,high_thr)
+
+MAD, precision, recall, F1_score = compute_metrics(hysteresis_image,cv2_edges)
+
+print(f"The MAD score between the two images is {MAD:.3f}")
+# print(f"The precision score between the two images is {precision:.3f}")
+# print(f"The recall score between the two images is {recall:.3f}")
+print(f"The F1_score score between the two images is {F1_score:.3f}")
+
+#8. Display original image, your edges, and OpenCV edges
+fig, axes = plt.subplots(1, 3, figsize=(10, 5))
+
+axes[0].imshow(img, cmap='gray')
+axes[0].set_title("Original Image")
+axes[0].axis('off')
+
+axes[1].imshow(hysteresis_image, cmap='gray')
+axes[1].set_title("Custom Edges")
+axes[1].axis('off')
+
+axes[2].imshow(cv2_edges, cmap='gray')
+axes[2].set_title("CV2 Edges")
+axes[2].axis('off')
+
+plt.tight_layout()
+plt.show()
