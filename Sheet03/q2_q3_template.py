a0042832811008018d17d9144183fab0dc96d219
Suraj <kuswahasuraj222@gmail.com>
2025-11-10 23:49:13 +0100

    completed task 3 and made some small changes

--- Sheet03/q2_q3_template.py
+++ Sheet03/q2_q3_template.py
@@ -32,13 +32,13 @@ def myHoughCircles(edges, min_radius, max_radius, threshold, min_dist, r_ssz, th
     h, w = edges.shape
 
     #Create the array of radius and angles to be considered
-    usable_radius = np.linspace(min_radius,max_radius,np.floor((max_radius-min_radius)/r_ssz).astype(int))
+    usable_radius = np.linspace(min_radius, max_radius, np.floor((max_radius - min_radius)/r_ssz).astype(int))
     number_radius = usable_radius.shape[0]
 
-    usable_angles=np.linspace(0,2*np.pi,theta_steps)
+    usable_angles=np.linspace(0, 2*np.pi, theta_steps, endpoint=False)
 
     #Create the matrix to store the votes
-    accumulator = np.zeros((h, w, number_radius))
+    accumulator = np.zeros((h, w, number_radius) ,dtype=np.float32)
 
     for point in edges_points:
         for r_idx, r in enumerate(usable_radius):
@@ -51,14 +51,14 @@ def myHoughCircles(edges, min_radius, max_radius, threshold, min_dist, r_ssz, th
 
             # Increment votes only for valid coordinates
             for a, b in zip(a_coords[valid_mask], b_coords[valid_mask]):
-                accumulator[a, b, r_idx] += 1
+                accumulator[a, b, r_idx] += 1.0
 
     #Whe threshold the accumulator to focus on strong candidates
-    vertical_coord, horizontal_coord, potential_r=np.where(accumulator>=threshold)
+    vertical_coord, horizontal_coord, potential_r = np.where(accumulator>=threshold)
 
     #We build a list that element wise stores the centre, radius and votes that have the elements
     #of our treshholded accumulator
-    votes=accumulator[vertical_coord,horizontal_coord,potential_r]
+    votes = accumulator[vertical_coord, horizontal_coord, potential_r]
 
     list_of_centres=np.transpose(np.stack([vertical_coord,horizontal_coord,potential_r,votes]))
     
@@ -67,54 +67,55 @@ def myHoughCircles(edges, min_radius, max_radius, threshold, min_dist, r_ssz, th
 
     detected_circles=[]
 
-    for y_coord, x_coord, rad_index, _ in list_of_centres:
+    for y_coord, x_coord, rad_index, vote_count in list_of_centres:
         #Until we check distance to previously accepted circles we assume is a new circle
         consider_circle=True 
 
         #Check distance to previous circles
         for circle in detected_circles:
-            if ((y_coord-circle[0])**2-(x_coord-circle[1])**2)<min_dist**2:
+            if ((y_coord - circle[0])**2 - (x_coord - circle[1])**2)<min_dist**2:
                 consider_circle=False
                 break
         
         if consider_circle:
-            detected_circles.append([y_coord,x_coord,usable_radius[rad_index.astype(int)],votes])
+            detected_circles.append([y_coord, x_coord, usable_radius[rad_index.astype(int)], vote_count])
 
 
 
     return  detected_circles, accumulator
 
-def drawCircles(detected_circles, height, width):
+
+def drawCirclesOnImage(img, peaks, radii_bins=None, color=(0, 255, 0), thickness=2):
+    """Draws circles on existed grayscale/BGR image
     """
-    Draws the circles encoded in detected_circles
 
-    detected_circles: Matrix obtained as a rasult of Hough transform.
-    h: height of the canvas
-    w: widht of the canvas
-
-    returns a matrix with the circles detected.
-    """
-    #We build the canvas
-    canvas=np.zeros([height, width])
-
-    #We set the r and the theta parameters
-    theta_steps = 120
-    usable_angles=np.linspace(0,2*np.pi,theta_steps)
+    # Converting grayscale to BGR so colors can be drawn
+    if len(img.shape) == 2:
+        vis_img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    else:
+        vis_img = img.copy()
     
-    for circle in detected_circles:
-        #Get the coordinates for the circles
-        a_coords = np.round(circle[0] + circle[2] * np.sin(usable_angles)).astype(int)
-        b_coords = np.round(circle[1] + circle[2] * np.cos(usable_angles)).astype(int)
-
-        #Check that the coordinate is in bounds
-        valid_mask = (a_coords >= 0) & (a_coords < height) & \
-                        (b_coords >= 0) & (b_coords < width)
-
-        #draw the circle
-        for a, b in zip(a_coords[valid_mask], b_coords[valid_mask]):
-                canvas[a, b] = 200
+    for c in peaks:
+        # Center coordinates
+        x = int(c[0])
+        y = int(c[1])
+        
+        if len(c) >= 3:
+            r_idx = int(c[2]) # radius index
+        else:
+            continue
+        # If radii_bins is provided, map r_idx -> true radius value
+        if radii_bins is not None:
+            r = int(radii_bins[r_idx])
+        else:
+            r = r_idx   # fallback if bins not provided
+        
+        # Draw main circle
+        cv2.circle(vis_img, (x, y), r, color, thickness)
+        # Draw a small dot at the center
+        cv2.circle(vis_img, (x, y), 2, (0,0,255), -1)
     
-    return canvas
+    return vis_img
 
 def myMeanShift(accumulator, bandwidth, threshold=None):
     """
@@ -128,6 +129,9 @@ def myMeanShift(accumulator, bandwidth, threshold=None):
     Returns:
         peaks: List of (x, y, r_idx, value) tuples
     """
+    
+    accumulator = np.transpose(accumulator, (2, 0, 1))
+    
     n_r, h, w = accumulator.shape
     
     # Computing threshold if None is passed as an argument
@@ -139,16 +143,30 @@ def myMeanShift(accumulator, bandwidth, threshold=None):
     
     # Collecting seed points
     seeds = np.argwhere(accumulator >= threshold)
-    if len(seeds) == 0:
+    MAX_SEEDS = 5000 # safey cap on the number of seeds because it over 2.2 million seeds
+    
+    # Keeping only the strongest seeds
+    if len(seeds) > MAX_SEEDS:
+        # Get accumulator values for all seed positions
+        vals = np.array([accumulator[int(r), int(y), int(x)] for (r, y, x) in seeds], dtype=float)
+        # Keep indices of top MAX_SEEDS values
+        keep = np.argpartition(vals, -MAX_SEEDS)[-MAX_SEEDS:]
+        
+        seeds = [seeds[i] for i in keep]
+    print("Total number of seeds:", len(seeds))
+    if not seeds:
         return []
     
     peaks = []
+    
+    max_iter = 30 # Upper bound on mean shift steps
+    stop_eps = 1e-2 # Stopping condition when movement is tiny
     
     # Mean shifting from each seed
     for r, y, x in seeds:
         r, y, x = float(r), float(y), float(x)
         
-        while True:
+        for _ in range(max_iter):
             # windows
             r_min = max(0, int(r - bandwidth))
             r_max = min(n_r - 1, int(r + bandwidth))
@@ -168,9 +186,9 @@ def myMeanShift(accumulator, bandwidth, threshold=None):
             
             #Squared distances
             dist2 = (r_axis - r)**2 + (y_axis - y)**2 + (x_axis - x)**2
-            window = (dist2 <= bandwidth * bandwidth).astype(float)
+            Wg = np.exp(-0.5 * (dist2 / max(float(bandwidth)**2 , 1e-12)))
             
-            weights = sub_accumulator * window
+            weights = sub_accumulator * Wg
             S = weights.sum()
             
             if S == 0:
@@ -181,10 +199,11 @@ def myMeanShift(accumulator, bandwidth, threshold=None):
             x_new = (weights * x_axis).sum() / S
             
             shift = np.sqrt((r_new - r)**2 + (y_new - y)**2 + (x_new - x)**2)
+            
             r, y , x = r_new, y_new, x_new
             
             #Convergence
-            if shift < 1e-3:
+            if shift < stop_eps:
                 break
         
         r, y, x = int(round(r)), int(round(y)), int(round(x))
@@ -230,9 +249,8 @@ def main():
     
     detect_circles , accumulator = myHoughCircles(img_edges, min_radius, max_radius, threshold, min_dist, r_ssz, theta_ssz)
 
-    h, w= img.shape
-
-    canvas_detection=drawCircles(detect_circles, h, w)
+    circles_for_draw = [(int(x), int(y), float(r), float(v)) for (y, x, r, v) in detect_circles] # Just converting it in the required format for visulaization
+    vis_canvas = drawCirclesOnImage(img_original, circles_for_draw, radii_bins=None)
 
     # Visualize detected circles
     fig, axes = plt.subplots(2, 3, figsize=(12, 4)) 
@@ -243,7 +261,7 @@ def main():
     axes[0][1].imshow(img_edges, cmap='gray')
     axes[0][1].set_title('Edge map')
 
-    axes[0][2].imshow(canvas_detection, cmap='gray')
+    axes[0][2].imshow(vis_canvas, cmap='gray')
     axes[0][2].set_title('Circles detected')
 
     axes[1][0].imshow(accumulator[:,:,10], cmap='gray')
@@ -284,14 +302,48 @@ def main():
     print("Task 3: Mean Shift for Peak Detection in Hough Accumulator")
     print("=" * 70)
 
+    bandwidth = 3
     print("Applying mean shift to find peaks...")
-    #peaks = myMeanShift(accumulator, bandwidth, threshold)
-    #print(f"Found {len(peaks)} raw peaks")
+    peaks = myMeanShift(accumulator, bandwidth, threshold)
+    print(f"Found {len(peaks)} raw peaks")
     
+    # Build the raidus value corresponding to the raidus index
+    usable_radius = np.linspace(
+    min_radius,
+    max_radius,
+    int(np.floor((max_radius - min_radius) / r_ssz)))
     
+    # Sorting peakes by vote strength
+    # This way, we keep the strongest detections before weaker duplicates
+    peaks.sort(key=lambda p: p[3], reverse=True)
+    filtered = []
     
+    # If two peaks are closer than this we consider them to be same circle
+    # 15px is chosen for this image scale, but can be tuned
+    min_center2 = 15**2
+    # Simple non maximum suppression on the detected circles
+    # We iterate through the peaks and keep only those peaks if it is not too close the previous one
+    for (x, y, r, v) in peaks:
+        keep = True
+        # Compare this peak center to previously accepted center locations
+        for (xx, yy, rr, vv) in filtered:
+            # Center too close -> duplicate -> skip
+            if (x-xx)**2 + (y-yy)**2 < min_center2:
+                keep = False
+                break
+        if keep:
+            filtered.append((x, y, r, v))
+
+    peaks = filtered
     # Visualize corresponding circles on original image    
-    # TODO
+    vis = drawCirclesOnImage(img, peaks, radii_bins=usable_radius)
+    
+    plt.figure(figsize=(7,7))
+    plt.imshow(cv2.cvtColor(vis, cv2.COLOR_BGR2RGB))
+    plt.title(f"Detected circles with Mean Shift (bandwidth={bandwidth}, peaks={len(peaks)})")
+    plt.axis('off')
+    plt.show()
+
     
     print("\n" + "=" * 70)
     print("Bandwidth Parameter Analysis:")
