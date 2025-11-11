21fb4d96e8b7c04fe0b7a46c49148f4d28cf1018
Suraj <kuswahasuraj222@gmail.com>
2025-11-11 15:34:33 +0100

    Added Sheet04

---
+++ Sheet04/01_segmentation.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+import matplotlib
+import skimage
+
+np.set_printoptions(suppress=True)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# TODO your implementation
+
